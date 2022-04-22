cdc4ef9eb5e7971eead473a794e9babf7d65df06
mathyus gomez <mathyusdgomez@gmail.com>
2022-04-22 11:05:41 -0500

    reto1

---
+++ scripts/scripts.js
@@ -0,0 +1,36 @@
+const porcentaje = 100 ;
+
+function validacion () {
+
+
+    let valor = document.getElementById("num1").value;
+    let propina = document.getElementById("num2").value;
+    let valorint = parseInt (valor) ;
+    let valorpro = parseInt (propina) ;
+    let valorpropina = (valorint * valorpro/porcentaje);
+    let totalapagar = (valorpropina + valorpro);
+    if ( valor === "" ||propina === ""){
+        alert("por favor ingrese los valores")
+        document.getElementById("limpiar").style.display = "none" ;
+     
+    } 
+    else {
+        document.getElementById('valor1').innerHTML = valor ;
+        document.getElementById('valor2').innerHTML = valorpropina ;
+        document.getElementById('totalapagar').innerHTML = totalapagar ;
+        document.getElementById("limpiar").style.display = "block"
+    }
+}
+//aqui se supone que debe
+function esconder(){
+    document.getElementById("limpiar").classList.add("mostrar");
+}
+document.getElementById("caja").onclick = function (){
+    esconder();
+}
+
+
+
+
+
+
